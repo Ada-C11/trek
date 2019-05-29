8ddcd944651419dcfa5ab1ccb35cdf5591d73058
Nidhi <nidhixpatel@gmail.com>
2019-05-29 16:55:19 -0700

    can display trek list

---
+++ index.js
@@ -0,0 +1,74 @@
+// index.js
+const URL = "https://trektravel.herokuapp.com/trips"
+
+let tripIDs = []
+
+const loadTrips = () => {
+
+  tripList = $('#trip-list');
+  tripList.empty();
+
+  axios.get(URL)
+    .then((response) => {
+      response.data.forEach((trip) => {
+        tripList.append(`<li id="${trip.id}">${trip.name}</li>`);
+        tripIDs.push("trip.id")
+        $("#" + trip.id).click(loadDetails(trip.id));
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+
+const loadDetails = (id) => {
+  const tripDetails = $('#detail-list');
+  tripDetails.empty()
+
+  axios.get(URL+"/"+id)
+  .then((response) => {
+      let titles = ["name", "continent", "about", "category", "weeks", "cost"]
+      titles.forEach((title) => {
+        tripDetails.append(`<li>${title}: ${response["data"][title]}</li>`);
+      })
+
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+  
+};
+
+$(document).ready(() => {
+  $('#load').click(loadTrips);
+});
+
+
+  // tripIDs.forEach((id) => {
+  //   $("#" + id).click(loadDetails);
+  // })
+
+
+  // $("#detail-button").click(loadDetails);
+
+
+// tripList.forEach((trip) => {
+//   $("#view-trip").click(loadTrips);
+// })
+
+// const loadDetails = (id) => {
+//   console.log("made it")
+//   const tripDetails = $('detail-list');
+//   tripDetails.empty()
+//   tripDetails.append(`<li>made it</li>`)
+
+//   axios.get(URL+"id")
+//   .then((response) => {
+//       tripDetails.append(`<li>made it</li>`);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+  
+// };
