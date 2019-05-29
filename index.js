78e805b950ab89f3f9361be2ab473d5c6431e7fe
Shirley Chu <shirley.cchu@gmail.com>
2019-05-29 15:08:21 -0700

    got trip details to show

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const loadTrips = () => {
       // parses through each response 
       response.data.forEach((trip) => {
         // adds each trip to list 
-        tripList.append(`<li> Trip ${trip.id} : ${trip.name} </li>`);
+        tripList.append(`<li class='trip' id=${trip.id}> Trip ${trip.id} : ${trip.name} </li>`);
       });
     })
     .catch((error) => {
@@ -43,11 +43,47 @@ const loadTrips = () => {
     });
 };
 
-// 
+// read into single trip 
+const readTrip = (tripID) => {
+  reportStatus('Loading trips...');
+
+  const tripAttributes = $('#trip-attributes');
+  tripAttributes.empty();
+
+  // compiles url for specific trip
+  const tripURL = tripListURL + '/' + tripID;
+
+  // sends GET request to endpoint
+  axios.get(tripURL)
+    .then((response) => {
+      reportStatus(`Successfully loaded Trip ${tripID}`);
+      // add header
+      tripAttributes.append(`<h1> Trip Details </h1>`);
+      // parse through hashy details object
+
+      for (let [detail, value] of Object.entries(response.data)) {
+        tripAttributes.append(`<li class='detail'> ${detail}: ${value} </li>`);
+      }
+      // response.data.forEach((detail) => {
+      //   // adds each trip to list 
+      //   tripAttributes.append(`<li class='detail'> ${detail} </li>`);
+      // });
+    })
+    .catch((error) => {
+      reportStatus(`Encountered an error while loading trips: ${error.message}`);
+      console.log(error);
+    });
+};
+
+// Object.entries documentation 
 
 //
 // OK GO!!!!!
 //
 $(document).ready(() => {
   $('#load').click(loadTrips);
+  $('#trip-list').on('click', '.trip', function () {
+    readTrip(this.id);
+  })
+
 });
