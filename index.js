9c4628dcaf7395f4d175454c831a5f2bf42a0746
kateannnichols <kateannnichols@gmail.com>
2019-05-29 18:03:37 -0700

    trip list displays - created empty element with trip list, axios sends the request from JS to trip API, .then loops through the response data, and displays the trip names (wave 1)

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+const tripsUrl = 'https://trektravel.herokuapp.com/trips';
+
 const displayStatus = (message) => {
     $('#status').html(message);
   }
@@ -7,12 +10,40 @@ const displayStatus = (message) => {
     // TODO: politely report this error to the user
   }
   
-  const loadTrips = () => {
-    displayStatus("loading trips...");
+//   const loadTrips = () => {
+//     displayStatus("loading trips...");
   
+//     axios.get(https://trektravel.herokuapp.com/trips)
+//     .then((response) => {
+//       response.data.forEach((pet) => {
+//         petList.append(`<li>${pet.name}</li>`);
+//       });
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
+
+const loadTrips = () => {
+    const tripList = $('#trip-list');
+    tripList.empty();
+    
+    axios.get(tripsUrl)
+      .then(function (response) {
+        response.data.forEach((trip) => {
+            tripList.append(`<li>${trip.name}</li>`);
+          });
+    
+          console.log(response);
+        // resultElement.innerHTML = generateSuccessHTMLOutput(response);
+      })
+      .catch(function (error) {
+          console.log(error);
+        // resultElement.innerHTML = generateErrorHTMLOutput(error);
+      });   
+  }
     // TODO: Wave 1
     // make an axios call to the trips index and display the results
-  }
   
   const showTripDetails = (trip) => {
     console.log("showing details for trip", trip);
