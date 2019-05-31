3a5a38114e47234172f453bb7cb4ab9e5d0e1486
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-31 11:24:02 -0700

    ammended structure of appended information and included hover action css

--- index.js
+++ index.js
@@ -3,32 +3,38 @@ const loadTrips = () => {
 
   // Prep work
   const URL = 'https://trektravel.herokuapp.com/trips'
-  const tripsList = $('#trips');
-  tripsList.empty();
+  // const tripsList = $('#trips');
+  // tripsList.empty();
+  $('section').empty();
 
   // setTimeout(() => {
     axios.get(URL)
     .then((response) => {
       // reportStatus(`Successfully loaded ${response.data.length} Trips`);
-      tripsList.append('<tr><th>All Trips</th></tr>')
+      $('section').append('<table id="trips"><tr><th>All Trips</th></tr></table>')
+
       response.data.forEach((trip) => {
             const loadOneTrip = () => {
               const URL = `https://trektravel.herokuapp.com/trips/${trip.id}` 
               axios.get(URL)
               .then((response) => {
                 $(`#${trip.id}`).append(`<ul id="info"><li>Name: ${trip.name}</li><li>Continent: ${trip.continent}</li><li>Category: ${trip.category}</li><li>Weeks: ${trip.weeks}</li><li>Cost: $${trip.cost}</li><li>About: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</li></ul>`);
-
+                // $(`#${trip.id}`).unbind('click', loadOneTrip);
               })
               .catch((error) => {
                 // reportStatus(`Encountered an error while loading pets: ${error.message}`);
                 console.log(error);
               });
             }
-        tripsList.append(`<tr><td id='${trip.id}'>${trip.name}</td></tr>`);
+        $('#trips').append(`<tr><td id='${trip.id}'>${trip.name}</td></tr>`);
+
+        $(`#${trip.id}`).hover(() => {
+          $(`#${trip.id}`).css('color', '#ff4949').css('font-weight', 'bold');
+        }, () => {
+          $(`#${trip.id}`).css('color', 'black').css('font-weight', 'normal');
+        });
         $(`#${trip.id}`).one('click', loadOneTrip);
       });
-
-      // tripsList.append(`<li>${response.data[0].display_name}: ${response.data[0].lat}, ${response.data[0].lon}</li>`);
     })
     .catch((error) => {
       // reportStatus(`Encountered an error while loading pets: ${error.message}`);
@@ -40,4 +46,3 @@ const loadTrips = () => {
 $(document).ready(() => {
   $('#load').click(loadTrips);
 })
-
