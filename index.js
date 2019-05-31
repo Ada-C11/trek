708d1d5eb5edd14f69c1e528f70a770f44c5f6e5
Angela <angela.ohh@gmail.com>
2019-05-31 15:09:52 -0700

    added background image

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TRIPSURL = 'https://trektravel.herokuapp.com/trips'
-// const ONETRIP = 'https://trektravel.herokuapp.com/trips/' 
-// TODO: need to concatenate id number at end of ONETRIP
+// const RESERVEURL = 'https://trektravel.herokuapp.com/trips/1/reservations'
+
 
 const reportStatus = (message) => {
     const statusContainer = $('#status-message');
@@ -12,12 +12,16 @@ const loadTrips = () => {
     const tripsList = $('#trip-list')
     tripsList.empty();
     tripsList.append('<h1>All Trips</h1>');
+    tripsList.addClass('border')
+    // $('main').addClass('background')
+
 
     axios.get(TRIPSURL)
         .then((response) => {
             reportStatus(`Successfully loaded ${response.data.length} trips!`);
             response.data.forEach((trip) => {
-                tripsList.append(`<li><button class='one-trip'>${trip.name}</button></li>`)
+                tripsList.append(`<li><button class='${trip.id}'>${trip.name}</button></li>`)
+                // $('li').addClass('border')
             })
         })
         .catch((error) => {
@@ -26,19 +30,28 @@ const loadTrips = () => {
         });
 }
 
-// How can we get ID from button clicked on?
 const loadDetails = (id) => {
     const tripDetail = $('.trip-details');
     tripDetail.empty();
     tripDetail.append('<h1>Trip Details</h1>')
+    tripDetail.addClass('border')
+
+    // const reserveTrip = $('.reserve-trip')
+    // reserveTrip.empty()
+    $('#reserve-trip').addClass('border')
 
     axios.get(TRIPSURL + `/${id}`)
         .then((response) => {
             let tripInfo = response.data
-            console.log(tripInfo)
             reportStatus(`Successfully loaded ${tripInfo.name} trips!`);
             tripDetail.append(`<h3>Name: ${tripInfo.name}</h3>`)
             tripDetail.append(`<h3>Continent: ${tripInfo.continent}</h3>`)
+            tripDetail.append(`<h3>Category: ${tripInfo.category}</h3>`)
+            tripDetail.append(`<h3>Weeks: ${tripInfo.week}</h3>`)
+            tripDetail.append(`<h3>Cost: $${tripInfo.cost}</h3>`)
+            tripDetail.append(`<p><h3>About:</h3> ${tripInfo.about}</p>`)
+
+            // reserveTrip.append(`<h1>Reserve a Spot on ${tripInfo.name} Trip!</h1>`)
         })
         .catch((error) => {
             reportStatus(`Encountered an error while loading trips: ${error.message}`);
@@ -46,11 +59,19 @@ const loadDetails = (id) => {
         })
 }
 
+// const readUserForm = () => {
+//     return {
+//         name: 'angela', 
+//         age: 4,
+//         email: 'angela@angela.org'
+//     }
+// }
 
   $(document).ready(() => {
     $('#load').on('click', loadTrips);
-    $('ul').on('click', 'button.one-trip', function() {
-        const tripId = $(this).html()
+    $('ul#trip-list').on('click', 'button', function() {
+        const tripId = this.className
         loadDetails(tripId)
+        $('#reserve-trip').show();
     });
   });
