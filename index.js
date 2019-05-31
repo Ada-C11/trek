3370a83f8ea2faa3ed421ca9e4255d3e8bc2da15
kateannnichols <kateannnichols@gmail.com>
2019-05-31 00:20:24 -0700

    can click on individual trips

--- index.js
+++ index.js
@@ -3,37 +3,35 @@ const tripsUrl = 'https://trektravel.herokuapp.com/trips';
 
 const displayStatus = (message) => {
     $('#status').html(message);
-  }
+  };
   
-  const handleApiError = (error) => {
-    console.log(error);
-    // TODO: politely report this error to the user
-  }
-  
-//   const loadTrips = () => {
-//     displayStatus("loading trips...");
-  
-//     axios.get(https://trektravel.herokuapp.com/trips)
-//     .then((response) => {
-//       response.data.forEach((pet) => {
-//         petList.append(`<li>${pet.name}</li>`);
-//       });
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
+  // const handleApiError = (error) => {
+  //   console.log(error);
+  //   // TODO: politely report this error to the user
+  // }
 
+  // const reportError = (message, errors) => {
+  //   let content = `<p>${message}</p><ul>`;
+  //   for (const field in errors) {
+  //     for (const problem of errors[field]) {
+  //       content += `<li>${field}: ${problem}</li>`;
+  //     }
+  //   }
+  //   content += "</ul>";
+  //   reportStatus(content);
+  // };
+
+// makes an axios call to the trips index and display the results
 const loadTrips = () => {
     const tripList = $('#trip-list');
     tripList.empty();
-    
+
     axios.get(tripsUrl)
       .then(function (response) {
         response.data.forEach((trip) => {
-            tripList.append(`<li>${trip.name}</li>`);
+            tripList.append(`<li><a href="" class="trip-link" id="${trip.id}">${trip.name}</a></li>`);
           });
-    
+
           console.log(response);
         // resultElement.innerHTML = generateSuccessHTMLOutput(response);
       })
@@ -42,15 +40,28 @@ const loadTrips = () => {
         // resultElement.innerHTML = generateErrorHTMLOutput(error);
       });   
   }
-    // TODO: Wave 1
-    // make an axios call to the trips index and display the results
-  
+
   const showTripDetails = (trip) => {
     console.log("showing details for trip", trip);
+
+    const tripDetails = $('#trip-details');
+    tripDetails.empty();
+
+    axios.get(tripsUrl)
+      .then(function (response) {
+        response.data.forEach((trip) => {
+          tripDetails.append(`<li>${trip.id}</li>`);
+        });
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });   
+    } 
   
     // TODO: Wave 2
     // display trip details and the trip reservation form
-  }
+  // }
   
   const reserveTrip = (trip) => {
     console.log("reserving trip", trip)
