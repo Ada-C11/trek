a4c170c92186480770e849fd0e6e22693a7d0f40
MyriamWD <myriam.waldend@gmail.com>
2019-05-31 15:37:52 -0700

    retrieving data per trip and displaying on trip details section

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const reportApiError = (error) => {
   reportStatus(errorHtml);
 }
 
-const loadPets = () => {
+const loadTrips = () => {
   reportStatus('Loading trips...');
 
   const tripsList = $('#trips-list');
@@ -32,47 +32,87 @@ const loadPets = () => {
     .then((response) => {
       reportStatus(`Successfully loaded ${response.data.length} trips`);
       response.data.forEach((trip) => {
-        tripsList.append(`<li>${trip.name}</li>`);
+        tripsList.append(`<li class=${trip.id}>${trip.name}</li>`);
+        // tripsList.append(`<li class=${trip.id}><a href=${URL}/${trip.id}>${trip.name}</a></li>`);
       });
     })
     .catch((error) => {
-      reportStatus(`Encountered an error while loading pets: ${error.message}`);
+      reportStatus(`Encountered an error while loading trips: ${error.message}`);
       console.log(error);
     });
 };
 
-const readPetForm = () => {
-  return {
-    // name: "dan's ports pet don't use this name please I need it for debugging",
-    age: 14,
-    owner: "ports"
-  };
+// add event listener to each link and capture the id to use in the get request
+// make the names in the list name a link using the id of the trip
+const loadIndividualTrip = (id)=> {
+    reportStatus('Loading trip...');
+    const tripDetails = $('.trip-details');
+    tripDetails.empty();
+
+    axios.get(`${URL}/${id}`)
+     .then((trip) => {
+         console.log(trip);
+       
+            tripDetails.append(`<li class="name">${trip.data.name}</li>`);
+            tripDetails.append(`<li class="continent">${trip.data.continent}</li>`);
+            tripDetails.append(`<li class="category">${trip.data.category}</li>`);
+            tripDetails.append(`<li class="weeks">${trip.data.weeks}</li>`);
+            tripDetails.append(`<li class="cost">${trip.data.cost}</li>`);
+            tripDetails.append(`<li class="about">${trip.data.about}</li>`);
+
+     })
+
+     
+    .catch((error) => {
+            reportStatus(`Encountered an error while loading trips: ${error.message}`);
+            console.log(error);
+ 
+
+     });
+
+
+    
 }
 
-const addPet = () => {
-  const petData = readPetForm();
+// const readPetForm = () => {
+//   return {
+//     // name: "dan's ports pet don't use this name please I need it for debugging",
+//     age: 14,
+//     owner: "ports"
+//   };
+// }
 
-  reportStatus("About to post pet data...");
-  console.log("About to post pet data", petData);
+// const addPet = () => {
+//   const petData = readPetForm();
 
-  axios.post(URL, petData)
-    .then((response) => {
-      console.log("successfully posted pet data", response);
+//   reportStatus("About to post pet data...");
+//   console.log("About to post pet data", petData);
 
-      const petId = response.data.id;
-      reportStatus(`Successfully created a new pet with ID ${petId}`);
-    })
-    .catch((error) => {
-      reportApiError(error);
-    })
-};
+//   axios.post(URL, petData)
+//     .then((response) => {
+//       console.log("successfully posted pet data", response);
+
+//       const petId = response.data.id;
+//       reportStatus(`Successfully created a new pet with ID ${petId}`);
+//     })
+//     .catch((error) => {
+//       reportApiError(error);
+//     })
+// };
 
 // OK GO!!!!!
 $(document).ready(() => {
-  $('#load').click(loadPets);
+  $('#load').click(loadTrips);
 
-  $('#pet-form').submit((event) => {
+  $('.current-trips').on('click', 'li', function(event){
+    let tripClass = this.className;
+    loadIndividualTrip(tripClass);
+  });
+//   $()loadIndividualTrip();
+
+  $('#reserve-form').submit((event) => {
     event.preventDefault();
-    addPet();
+    // addPet();
+
   });
 });
