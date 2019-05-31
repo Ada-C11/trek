cc3d8d9166156dd7185fce8e585c1b67ca5727d7
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-31 15:54:09 -0700

    initial HTML setup and start for list trips

---
+++ index.js
@@ -0,0 +1,25 @@
+const URL = 'https://trektravel.herokuapp.com/trips';
+
+const reportStatus = (message) => {
+    $('#status-message').html(message);
+};
+
+const listTrips = () => {
+    reportStatus('Loading trips...');
+
+    const tripList = $('#trip-list');
+    tripList.empty();
+
+    axios.get(URL)
+        .then((response) => {
+            reportStatus(`Successfully loaded ${response.data.length} trips`);
+            response.data.forEach((trip) => {
+                tripList.append(`<li>${trip.name}</li>`);
+            });
+        })
+        .catch((error) => {
+            reportStatus(`Encountered an error while loading pets: ${error.message}`);
+            console.log(error);
+        });
+};
+
