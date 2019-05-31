dfd11762cebad8a13edda629dedaa039345994f9
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-31 10:30:29 -0700

    structure of data when clicked changed to table per provided wireframe

--- index.js
+++ index.js
@@ -10,20 +10,21 @@ const loadTrips = () => {
     axios.get(URL)
     .then((response) => {
       // reportStatus(`Successfully loaded ${response.data.length} Trips`);
-      
+      tripsList.append('<tr><th>All Trips</th></tr>')
       response.data.forEach((trip) => {
             const loadOneTrip = () => {
               const URL = `https://trektravel.herokuapp.com/trips/${trip.id}` 
               axios.get(URL)
               .then((response) => {
-                $(`#${trip.id}`).append(`<ul id=info${trip.id}><li>Continent: ${trip.continent}</li><li>About: ${trip.about}</li><li>Category: ${trip.category}</li><li>Weeks: ${trip.weeks}</li><li>Cost: $${trip.cost}</li></ul>`)
+                $(`#${trip.id}`).append(`<ul id="info"><li>Name: ${trip.name}</li><li>Continent: ${trip.continent}</li><li>Category: ${trip.category}</li><li>Weeks: ${trip.weeks}</li><li>Cost: $${trip.cost}</li><li>About: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</li></ul>`);
+
               })
               .catch((error) => {
                 // reportStatus(`Encountered an error while loading pets: ${error.message}`);
                 console.log(error);
               });
             }
-        tripsList.append(`<li id='${trip.id}'>${trip.id}. ${trip.name}</li>`)
+        tripsList.append(`<tr><td id='${trip.id}'>${trip.name}</td></tr>`);
         $(`#${trip.id}`).one('click', loadOneTrip);
       });
 
