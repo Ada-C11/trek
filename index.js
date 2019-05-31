b97386ca376fc81df41d0dbf9552d8da57948eeb
rosalyn <rosalyn.anuseh@gmail.com>
2019-05-31 09:27:25 -0700

    opps not commiting enough

---
+++ index.js
@@ -0,0 +1,154 @@
+$(document).ready(function() {
+  listTrips();
+  loadTripDetails();
+});
+
+const getURL = "https://trektravel.herokuapp.com/trips";
+
+const reportStatus = message => {
+  $("#status-message").html(message);
+};
+
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p><ul>`;
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+};
+
+// This loads the trip
+const getTrips = () => {
+  reportStatus("Loading trips...");
+
+  const tripList = $("#trip-list");
+  tripList.empty();
+
+  axios
+    .get(getURL)
+    .then(response => {
+      reportStatus(`Successfully loaded ${response.data.length} trips`);
+      response.data.forEach(trip => {
+        tripList.append(`<li class='${trip.id}'>${trip.name}</li>`);
+      });
+    })
+    .catch(error => {
+      reportStatus(
+        `Encountered an error while loading trips: ${error.message}`
+      );
+      console.log(error);
+    });
+};
+
+// This gets the trip details given the id as a parameter
+const detailsTrips = function detailsTrips(id) {
+  reportStatus("Loading details...");
+
+  const tripDetailsList = $("#trip-details");
+  tripDetailsList.empty();
+
+  axios
+    .get(getURL)
+    .then(response => {
+      reportStatus(`Successfully loaded ${response.data.length} trips`);
+      // callback function which takes the id passed to it and finds the trip associated with that id
+      const trip = response.data.find(function(item) {
+        // returns true when that id matches the id passed to this method
+        return item.id.toString() === id;
+      });
+      loadForm(trip.name);
+      tripDetailsList.append(
+        `<h2>TRIP DETAILS</h2>`,
+        `<li>ID: ${trip.id}</li>`,
+        `<li>NAME: ${trip.name}</li>`,
+        `<li>CONTINENT: ${trip.continent}</li>`,
+        `<li>CATEGORY: ${trip.category}</li>`,
+        `<li>DURATION ${trip.weeks} weeks</li>`,
+        `<li>COST $${trip.cost.toFixed(2)}</li>`,
+        `<li>ABOUT: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '</li>`
+      );
+    })
+    .catch(error => {
+      reportStatus(
+        `Encountered an error while loading trips: ${error.message}`
+      );
+      console.log(error);
+    });
+};
+
+// Loads the form and calls the submit fuction
+function loadForm(trip) {
+  reportStatus("Loading details...");
+  const tripReservation = $(".reserve-trip-area");
+  tripReservation.empty();
+
+  tripReservation.append(
+    `<h2>Reserve a Spot on ${trip}</h2>
+    <form> Name:<br />
+      <input class="text-input" type="text" name="name" value="Mickey" />
+      <br />
+      Email:<br />
+      <input class="email-input" type="text" name="email" value="mickey@mouse.com" />
+      <br /><br />
+      <input class="submit-click" type="button" value="Submit" />
+    </form>`
+  );
+  submitForm();
+}
+
+const postURL = "https://trektravel.herokuapp.com/trips/1/reservations";
+
+// Posts the data submitted in the form
+function postForm(name, email) {
+  event.preventDefault();
+  axios({
+    method: "post",
+    url: postURL,
+    data: {
+      name: name,
+      email: email
+    }
+  }).catch(error => {
+    if (error.response.data && error.response.data.errors) {
+      reportError(
+        `Encountered an error while loading trips: ${error.message}`,
+        error.response.data.errors
+      );
+    } else {
+      reportStatus(`Encountered an error: ${error.message}`);
+    }
+  });
+}
+
+// Loads all available trips on click
+function listTrips() {
+  $("#load").click(getTrips);
+}
+
+// On clicking a specific trip give the details of that trip
+function loadTripDetails() {
+  $("#trip-list").click(function(event) {
+    const selected = event.target;
+    const trip = selected.classList[0];
+    detailsTrips(trip);
+  });
+}
+
+// Submits the form with your input on click
+function submitForm() {
+  $(".submit-click").click(function() {
+    const name = $(`.text-input`).val();
+    const email = $(`.email-input`).val();
+    postForm(name, email);
+    clearForm();
+  });
+}
+
+// Clears the form after submission
+const clearForm = () => {
+  $(`.text-input`).val(" ");
+  $(`.email-input`).val(" ");
+};
