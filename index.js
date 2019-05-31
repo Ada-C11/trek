c8bd96d62b5de279d6abdc34841cecc598beb812
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-30 17:34:35 -0700

    completed .then portion of get request - can now show list of trips

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const loadTrips = () => {
     tripList.empty();
 
     axios.get(url)
+        .then((response) => {
+            console.log('successfully loaded trips');
+            response.data.forEach((trip) => {
+                tripList.append(`<li>${trip.name}</li>`)
+            })
+        })
 }
 
 $(document).ready(() => {
     $('#load-trips').click(loadTrips);
-  });
+});
