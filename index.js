43eaa3e531cc54d4c2328629501000ec3a11888c
Heather Izumi <hkizumi@gmail.com>
2019-05-31 16:16:08 -0700

    Shows trip details

--- index.js
+++ index.js
@@ -1,84 +1,72 @@
 const ALL_URL = "https://trektravel.herokuapp.com/trips";
-const TRIP_URL = `https://trektravel.herokuapp.com/trips/${this.id}`
+
 
 const reportStatus = (message) => {
     const statusContainer = $('#status-message');
     statusContainer.empty();
     statusContainer.append(`<p>${message}</p>`);
-  };
+};
 
 const loadTrips = () => {
     reportStatus('Loading trips...');
-  
+
     const tripList = $('#trip-list');
     tripList.empty();
-  
-    axios.get(ALL_URL)
-      .then((response) => {
-        reportStatus(`Successfully loaded ${response.data.length} trips`);
-  
-        const trips = response.data;
-        trips.forEach((trip) => {
-            tripList.append(`<li>${trip.name}</li>`);
-        });
-    })
-    .catch((error) => {
-        // reportStatus(`Encountered an error while loading trips: ${error.message}`);
-        console.log('we made an api request, and it came back as "failure"!');
-        console.log('The error was this:', error);
-    });
-    }
 
-    const oneTrip = () => {
-        reportStatus('Loading trip...');
-      
-        const showTrip = $('.trip-details');
-        showTrip.empty();
-      
-        
-        // axios.get(ALL_URL, {
-        //     params: {
-        //       ID: 70
-        //     }
-        //   })
-        
-        
-        axios.get(ALL_URL)
-          .then((response) => {
+    axios.get(ALL_URL)
+        .then((response) => {
             reportStatus(`Successfully loaded ${response.data.length} trips`);
-      
-            const singleTrip = response.data;
-            showTrip.append(`<li>${singleTrip['about']}</li>`);
-           
+
+            const trips = response.data;
+            trips.forEach((trip) => {
+                const tripNode = $(`<li>${trip.name}</li>`);
+                tripNode.attr("data-id", trip.id);
+                tripList.append(tripNode);
+            });
         })
         .catch((error) => {
-            // reportStatus(`Encountered an error while loading trips: ${error.message}`);
-            console.log('we made an api request, and it came back as "failure"!');
-            console.log('The error was this:', error);
+            reportStatus(`Encountered an error while loading trips: ${error.message}`);
+
         });
-        }
+
+    const oneTrip = function oneTrip(event) {
+        reportStatus('Loading trip...');
+
+        const showTrip = $('#trip-details ul');
+        showTrip.empty();
+        const tripId = $(event.currentTarget).attr('data-id');
+        axios.get(`https://trektravel.herokuapp.com/trips/${tripId}`)
+            .then((response) => {
+                reportStatus('Retrieving trip details.');
+
+                const singleTrip = response.data;
+                
+                showTrip.append(`<li>Name: ${singleTrip['name']}</li>`);
+                showTrip.append(`<li>Continent: ${singleTrip['continent']}</li>`);
+                showTrip.append(`<li>About: ${singleTrip['about']}</li>`);
+                showTrip.append(`<li>Category: ${singleTrip['category']}</li>`);
+                showTrip.append(`<li>
+                Duration (weeks): ${singleTrip['weeks']}</li>`);
+                showTrip.append(`<li>Cost: $${singleTrip['cost']}</li>`);
+
+            })
+            .catch((error) => {
+                reportStatus('Details for this trip are not currently avialable.');
+            });
+
+    }
+
+    $('#trip-list').on('click', 'li', oneTrip)
+}
 
 
+$(document).ready(() => {
 
-
-  $(document).ready(() => {
-  
     $('#load').click(() => {
-      loadTrips();
+        loadTrips();
     });
 
-    $('#trip-list').on('click', 'li', function(event) {
-        oneTrip();
-      });
-    
-
-    // $('h1').click(() => {
-    //     // oneTrip(this.id);
-    //     oneTrip();
+    // $('button#load').click(function () {
+    //     $(this).hide();
     // });
-
-    $('button#load').click(function(){
-        $(this).hide();
-      });
-  
-  });
+});
