dc625bbd2b94de2cf35bc8dbc8a9348692b67c4a
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-30 17:44:30 -0700

    added a report status for users

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const url = 'https://trektravel.herokuapp.com/trips';
 
+const reportStatus = (message) => {
+    $('#status-message').html(message);
+};
+
 const loadTrips = () => {
+    reportStatus('...Loading all trips...');
+
     const tripList = $('#trip-list');
     tripList.empty();
 
     axios.get(url)
         .then((response) => {
+            reportStatus(`Successfully loaded ${response.data.length} trips`);
             console.log('successfully loaded trips');
             response.data.forEach((trip) => {
                 tripList.append(`<li>${trip.name}</li>`)
             })
+        })
+        .catch((error) => {
+            reportStatus(`We're sorry, we encountered an error while loading all trips: ${error.message}`);
+            console.log('there was an issue loading some or all of the trips');
         })
 }
 
