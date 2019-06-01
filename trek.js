0d5e879fbb3ec58154b7b40e2ef1954401199e28
Amy Phung <phung.amy85@gmail.com>
2019-06-01 13:30:24 -0700

    update for consistent use of double quotes

--- trek.js
+++ trek.js
@@ -1,7 +1,7 @@
 const baseURL = "https://trektravel.herokuapp.com/trips";
 
-const displayStatus = (message) => {
-  $('#status').html(message);
+const displayStatus = (message, element) => {
+  $(element).html(message);
 }
 
 const handleApiError = (error) => {
@@ -17,19 +17,19 @@ const handleApiError = (error) => {
       content += `<li><strong>${field}:</strong> ${problem}</li>`;
     });
   });
-  content += '</ul>';
-  displayStatus(content);
+  content += "</ul>";
+  displayStatus(content, "#status");
 }
 
 const loadTrips = () => {
-  displayStatus("loading trips...");
+  displayStatus("Loading trips...", "#status");
 
-  const tripList = $('#trip-list');
+  const tripList = $("#trip-list");
   tripList.empty();
 
   axios.get(baseURL)
     .then((response) => {
-      displayStatus(`Successfully loaded ${response.data.length} trips`);
+      displayStatus(`Successfully loaded ${response.data.length} trips`, "#status");
       response.data.forEach((trip) => {
         tripList.append(`<li><a href="#" data-trip-id=${trip.id}> ${trip.name}</a></li>`);
       });
@@ -41,11 +41,11 @@ const loadTrips = () => {
 }
 
 const formatTripDetails = (tripDetails) => {
-  const target = $('#trip-details-list');
+  const target = $("#trip-details-list");
   target.empty();
 
   Object.keys(tripDetails).forEach(function (detail) {
-    target.append(`<li><strong>${detail}:</strong> ${tripDetails[detail]}</li>`);
+    target.append(`<li id="capitalize"><strong>${detail}:</strong> ${tripDetails[detail]}</li>`);
   });
 };
 
@@ -53,12 +53,12 @@ const showTripDetails = (event) => {
   event.preventDefault();
 
   console.log("showing details for trip", $(event.target).html());
-  const byIdUrl = (baseURL + '/' + `${$(event.target).data("trip-id")}`);
+  const byIdUrl = (baseURL + "/" + `${$(event.target).data("trip-id")}`);
 
   axios.get(byIdUrl)
     .then((response) => {
       formatTripDetails(response.data);
-      $('#reserve-trip-form').addClass(`${response.data.id}`)
+      $("#reserve-trip-form").addClass(`${response.data.id}`)
         .submit(reserveTrip);
       $("#reserve-trip").show();
     })
@@ -80,23 +80,23 @@ const readFormData = () => {
 };
 
 const clearForm = () => {
-  $(`#reserve-trip-form input[name="name"]`).val('');
-  $(`#reserve-trip-form input[name="email"]`).val('');
+  $(`#reserve-trip-form input[name="name"]`).val("");
+  $(`#reserve-trip-form input[name="email"]`).val("");
 }
 
 const reserveTrip = (event) => {
-  const tripID = parseInt($('#reserve-trip-form').attr('class'));
+  const tripID = parseInt($("#reserve-trip-form").attr("class"));
 
-  displayStatus('Reserving your spot...');
+  displayStatus("Reserving your spot...", "#status");
 
-  const reservationUrl = (baseURL + '/' + tripID + '/reservations');
+  const reservationUrl = (baseURL + "/" + tripID + "/reservations");
   const reservationParams = readFormData();
   event.preventDefault();
 
   axios.post(reservationUrl, reservationParams)
     .then((response) => {
       const resId = response.data.id;
-      displayStatus(`Successfully reserved your spot! Your reference number is ${resId}!`);
+      displayStatus(`Successfully reserved your spot! Your reference number is ${resId}!`, "#reservation-status");
       clearForm();
     })
     .catch((error) => {
