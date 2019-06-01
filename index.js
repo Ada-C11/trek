ffbf478d789a083b10151c5ab0f5983619b5246a
K <774162+kaseea@users.noreply.github.com>
2019-05-31 22:54:00 -0700

    end of wave 2 but without a second api call, or closure

--- index.js
+++ index.js
@@ -43,19 +43,42 @@ const loadTrips = () => {
     axios.get(getTrips)
       .then((response) => {
         reportStatus(`Successfully loaded ${response.data.length} trips`);
+ 
         response.data.forEach((trip) => {
-          tripList.append(`<li>${trip.name}</li>`);
+
+          let holderName = `<li>${trip.name}`;
+          let id = `load_info_${trip.id}`;
+          // almsot all the fucking data is here
+          // how can I add it from here
+          // alert(id);
+          // this just runs alert non stop CAUSE I CALLED ALERT
+          // $(`#` + id).click(alert(id));
+          // when you have something in parans, the things in parens RUNS RIGHT AWAY
+          // CALLED A CONTINUATION OR A THUNK
+          holderName += `<div class="${id}"></div><button type="button" id="${id}" class="btn btn-info">more info</button>`;
+          // const holderNode = $(holderName)
+          // holderNode.click(function is the thing that displays it)
+          tripList.append(holderName);
+          
+          $(`#` + id).click(() => {
+            alert(id);
+            let deets = `<li>trip id: ${trip.id}, trip name: ${trip.name}, continent: ${trip.continent}, category: ${trip.category}, duration: ${trip.weeks}, cost: ${trip.cost} </li>`;
+            $(`.` + id).html(deets);
+          })
         });
+        // $(`#` + id).click(alert(id));
       })
       .catch((error) => {
         reportStatus(`Encountered an error while loading trips: ${error.message}`);
         console.log(error);
       });
+      // $(`#` + id).click(alert(id));
   };
 
 
   $(document).ready(() => {
     $('#load').click(loadTrips);
+
   });
 
 //   lots of credit goes to Dan's Aweosme live coding
