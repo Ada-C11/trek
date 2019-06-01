825030a52986ba8c8df19fc0df30fc010fb1b32e
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-06-01 15:47:55 -0700

    makeReservation works

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ const reportStatus = (message) => {
     $('#status-message').html(message);
 };
 
+const reportError = (message, errors) => {
+    let content = `<p>${message}</p>`
+    content += "<ul>";
+    for (const field in errors) {
+      for (const problem of errors[field]) {
+        content += `<li>${field}: ${problem}</li>`;
+      }
+    }
+    content += "</ul>";
+    reportStatus(content);
+  };
+
+const jsonifyFormData = (tripArray) => {
+    let returnObj = {}
+    for (let i = 0; i < tripArray.length; i += 1) {
+        console.log("got to method");
+        returnObj[tripArray[i]['name']] = tripArray[i]['value'];
+    }
+    return returnObj;
+};
+
+const clearForm = () => {
+    $('#trip-form').get(0).reset();
+};
+
 const loadTrips = () => {
     reportStatus('Loading trips...');
 
@@ -17,6 +42,9 @@ const loadTrips = () => {
             reportStatus(`Successfully loaded ${response.data.length} trips`);
             response.data.forEach((trip) => {
                 tripList.append(`<li id='${trip.id}'>${trip.name}</li>`);
+                $(`#${trip.id}`).click(function () {
+                    showTrip(trip.id);
+                });
             });
         })
         .catch((error) => {
@@ -27,10 +55,17 @@ const loadTrips = () => {
 
 const showTrip = (index) => {
     $('#status-message').html('');
-    console.log(index);
     axios.get(URL + `/${index}`)
         .then((response) => {
+            $('#form-section').removeClass('hidden');
+            $('#trip-form').submit(function (event) {
+                $('#tripID').val(response.data.id);
+                event.preventDefault();
+                makeReservation();
+            });
+
             $('#trip-details').removeClass('hidden');
+
             $('#name').html(`Trip ${response.data.id}: ${response.data.name}`);
             $('#continent').html(`<strong>Continent:</strong> ${response.data.continent}`)
             $('#category').html(`<strong>Category:</strong> ${response.data.category}`)
@@ -49,10 +84,43 @@ const showTrip = (index) => {
         });
 };
 
-$(document).ready(() => {
-    $('#load').click(loadTrips);
-    $('#trips-list').on('click', 'li', function (event) {
-        showTrip(event.target.id);
+const makeReservation = () => {
+    // const tripArray = $('#trip-form').serializeArray();
+    // const tripData = jsonifyFormData(tripArray);
+    // console.log(tripData);
+    const name = $('#name').val();
+    const email = $('#email').val();
+    const age = $('#age').val();
+    const tripID = $('#tripID').val();
+
+    const tripData = {
+        name: name,
+        email: email,
+        age: age,
+    }
+
+    axios.post(URL+`/${tripID}/reservations`, tripData)
+    .then((response) => {
+        clearForm();
+      console.log(response);
+      reportStatus('Successfully made reservation!');
+    })
+    .catch((error) => {
+      console.log(error.response);
+      if (error.response.data && error.response.data.errors) {
+        // User our new helper method
+        reportError(
+            `Encountered an error: ${error.message}`,
+            error.response.data.errors
+        );
+    } else {
+        // This is what we had before
+        reportStatus(`Encountered an error: ${error.message}`);
+    }
     });
 
+}
+
+$(document).ready(() => {
+    $('#load').click(loadTrips);    
 });
