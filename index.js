a5b81bb0b3fde18957e10f11352f26c837562223
Chantal Demissie <chantaldemissie@yahoo.com>
2019-05-31 22:24:46 -0700

    wave 2-ish

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
+let trips = {};
+
 const loadTrips = () => {
   reportStatus('Loading trips...');
 
@@ -14,7 +16,11 @@ const loadTrips = () => {
     .then((response) => {
       reportStatus(`Successfully loaded ${response.data.length} trips`);
       response.data.forEach((trip) => {
-        tripList.append(`<li>${trip.name}</li>`);
+        const listItem = $(`<li>${trip.name}</li>`).appendTo(tripList);
+        listItem.click(() => {
+          showTripDetails(trip.name);
+        })
+        trips[trip.name] = trip;
       });
     })
     .catch((error) => {
@@ -22,6 +28,18 @@ const loadTrips = () => {
       console.log(error);
     });
 };
+
+function showTripDetails(tripName) {
+  const trip = trips[tripName];
+  const details = $('#details');
+  details.append(`<p><b>Name</b>: ${trip.name}</p>`);
+  details.append(`<p>Continent: ${trip.continent}</p>`);
+  details.append(`<p>Category: ${trip.category}</p>`);
+  details.append(`<p>Weeks: ${trip.weeks}</p>`);
+  details.append(`<p>Cost: $${trip.cost}</p>`);
+  details.append(`<p>About:</p>`);
+  details.append(`<p>I wish I worked at Google.</p>`);
+}
 
 const createTrip = (event) => {
   event.preventDefault();
@@ -40,16 +58,16 @@ const createTrip = (event) => {
 };
 
 
-const details.html(
-  <h2>Trip Details</h2>
-  <h1>Trip ID</h1>
-  <h1>Trip Name</h1>
-  <h1>Continent</h1>
-  <h1>Details about the trip</h1>
-  <h1>Category of the trip</h1>
-  <h1>Number of weeks duration of the trip</h1>
-  <h1>Cost of the trip</h1>
-)
+// const details.html(
+//   <h2>Trip Details</h2>
+//   <h1>Trip ID</h1>
+//   <h1>Trip Name</h1>
+//   <h1>Continent</h1>
+//   <h1>Details about the trip</h1>
+//   <h1>Category of the trip</h1>
+//   <h1>Number of weeks duration of the trip</h1>
+//   <h1>Cost of the trip</h1>
+// )
 
 $(document).ready(() => {
   $('#load').click(loadTrips);
