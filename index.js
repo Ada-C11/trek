1d99c08d418897018173e0f1ce6656cde6156c4f
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-06-01 16:41:05 -0700

    removed unused code added intro portion for post request

--- index.js
+++ index.js
@@ -17,11 +17,13 @@ const loadTrips = () => {
             const loadOneTrip = () => {
               if ($('#trip').children().length > 0) {
                 $('#trip').empty();
+                $('#form').empty();
               }
               const URL = `https://trektravel.herokuapp.com/trips/${trip.id}` 
               axios.get(URL)
               .then((response) => {
-                $(`#trip`).append(`<table><tr><th>Trip Details</th></tr><tr><td><b>Name:</b> ${trip.name}</td></tr><tr><td><b>Continent:</b> ${trip.continent}</td></tr><tr><td><b>Category:</b> ${trip.category}</td></tr><tr><td><b>Weeks:</b> ${trip.weeks}</td></tr><tr><td><b>Cost:</b> $${trip.cost}</td></tr><tr><td><b>About:</b> Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</td></tr></table>`);
+                $('#trip').append(`<table><tr><th>Trip Details</th></tr><tr><td><b>Name:</b> ${trip.name}</td></tr><tr><td><b>Continent:</b> ${trip.continent}</td></tr><tr><td><b>Category:</b> ${trip.category}</td></tr><tr><td><b>Weeks:</b> ${trip.weeks}</td></tr><tr><td><b>Cost:</b> $${trip.cost}</td></tr><tr><td><b>About:</b> Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</td></tr></table>`);
+                $('#form').append('hello');
               })
               .catch((error) => {
                 reportStatus(`Encountered an error while loading pets: ${error.message}`);
@@ -31,31 +33,18 @@ const loadTrips = () => {
         $('#trips').append(`<tr><td id='${trip.id}'>${trip.name}</td></tr>`);
 
         $(`#${trip.id}`).one('click', loadOneTrip);
-        // $(`#${trip.id}`).one('click', loadOneTrip);
-        // $(`#${trip.id}`).one(() => {'click', $( "#trip" ).remove()}, () => {'click', loadOneTrip});
-        
-        // highlightColor()});
-
-
 
         $(`#${trip.id}`).hover(() => {
           $(`#${trip.id}`).css({'color': '#ff4949', 'font-weight': 'bold'});
         }, () => {
           $(`#${trip.id}`).css({'color': 'black', 'font-weight': 'normal'});
         });
-
-        // function clickTrip() {
-        //   $(`#${trip.id}`).one('click', loadOneTrip);
-        //   // $(`#${trip.id}`).css({'color': '#ff4949', 'font-weight': 'bold'});
-        // }
-        // $(`#${trip.id}`).one('click', () => {loadOneTrip, $(`#${trip.id}`).css({'color': '#ff4949', 'font-weight': 'bold'})});
       });
     })
     .catch((error) => {
       reportStatus(`Encountered an error while loading pets: ${error.message}`);
       console.log(error);
     });
-  // }, i*1000);
 }
 
 $(document).ready(() => {
