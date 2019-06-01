08f63678f96deb80970f948e0e224ae6d701aad5
Margaret Finnan <msfinnan@gmail.com>
2019-06-01 15:44:06 -0700

    worked on post request for reservatins but it is not working. Light styling to match wire frames

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const ALL_TRIPS_URL = 'https://trektravel.herokuapp.com/trips';
-// const ONE_TRIP = `https://trektravel.herokuapp.com/trips/`
+const BASE_URL = 'https://trektravel.herokuapp.com/trips';
 
 const displayStatus = (message) => {
   $('#status').html(message);
@@ -8,42 +7,26 @@ const displayStatus = (message) => {
 let tripId;
 
 const handleApiError = (error) => {
-  console.log(error);
+  console.log('Error:', error);
   // TODO: politely report this error to the user
 }
 
-// const currentTripId = (id) => {
-//   let currentId = id;
-
-//   const returnStoredId = () => {
-//     console.log(`This is trip number ${currentId}`);
-//     return currentId;
-//   }
-
-//   return returnStoredId;
-// };
 
 const loadTrips = () => {
-  displayStatus("loading trips...");
+  displayStatus("Loading trips...");
 
-  // TODO: Wave 1
-  // make an axios call to the trips index and display the results
-  axios.get(ALL_TRIPS_URL)
-
+  axios.get(BASE_URL)
     .then((response) => {
       const trips = response.data;
       displayStatus(`Successfully loaded ${trips.length} trips`);
-      console.log(trips)
       trips.forEach((trip) => {
 
         $('#trip-list').append(`<li><button id=${trip.id}>${trip.id}: ${trip.name}</button></li>`);
-        
-        
       });
     })
     .catch((error) => {
       displayStatus(`Encountered an error while loading tips: ${error.message}`);
-      console.log('The error was this:', error);
+      // console.log('The error was this:', error);
     });
 }
 
@@ -51,40 +34,99 @@ const showTripDetails = () => {
   // console.log('Im in the request!');
   // console.log(`the value of id currently is`);
   // console.log(`the value of id is`, tripId())
-  axios.get(`${ALL_TRIPS_URL}/${tripId}`) //need to get access to trip id at this time 
+  axios.get(`${BASE_URL}/${tripId}`) //need to get access to trip id at this time 
 
     .then((response) => {
       const trip = response.data;
       displayStatus('Successfully loaded trip');
       console.log(response.data);
+      $('#trip-details').empty();
+      $('#trip-details').append('<h2>Trip Details</h2>');
       $('#trip-details').append(`<li>Name: ${trip.name}</li>`);
       $('#trip-details').append(`<li>Continent: ${trip.continent}</li>`);
       $('#trip-details').append(`<li>Category: ${trip.category}</li>`);
       $('#trip-details').append(`<li>Cost: ${trip.cost}</li>`);
       $('#trip-details').append(`<li>About: ${trip.about}</li>`);
+      $('#trip-details').append(`<form id="trip-form">
+        <h1>Reserve a Spot on ${trip.name}</h1>
+        <div>
+          <label for="name">Name</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label for="email">Email</label>
+          <input type="text" name="email" />
+        </div>
+
+        <input type="submit" name="submit" value="Submit" />
+      </form>`)
+
     })
     .catch((error) => {
-      displayStatus(`Encountered an error while loading pets: ${error.message}`);
+      displayStatus(`Encountered an error while loading trip: ${error.message}`);
       console.log('The error was this:', error);
     });
 }
 
-const reserveTrip = (trip) => {
-  console.log("reserving trip", trip)
+const readFormData = () => {
+  const parsedFormData = {};
 
-  // TODO: Wave 2
-  // reserve a spot on the trip when the form is submitted
+  const nameFromForm = $(`#trip-form input[name="name"]`).val();
+  parsedFormData['name'] = nameFromForm ? nameFromForm : undefined;
+
+  const emailFromForm = $(`#trip-form input[email="email"]`).val();
+  parsedFormData['email'] = emailFromForm ? emailFromForm : undefined;
+
+  return parsedFormData;
+};
+
+const clearForm = () => {
+  $(`#trip-form input[name="name"]`).val('');
+  $(`#trip-form input[email="email"]`).val('');
 }
+
+const createReservation = () => {
+  // console.log("reserving", trip)
+
+  // event.preventDefault();
+
+  const reservationData = readFormData();
+  console.log("reservation data is", reservationData);
+  // console.log(`tripIDs`, tripId);
+
+  // displayStatus('Sending reservation data...');
+
+  axios.post(BASE_URL + '/' + tripId +'/reservations', reservationData)
+    .then((response) => {
+      console.log('posted res data', response);
+      const resId = response.data.id
+      displayStatus(`Successfully created a reservation with id of ${resId}`);
+      clearForm();
+    })
+    .catch((error) => {
+      console.log(error.response);
+      if (error.response.data && error.response.data.errors) {
+        // reportError(
+        //   `Encountered an error: ${error.message}`,
+        //   error.response.data.errors
+        // );
+        console.log(error)
+      } else {
+        displayStatus(`Encountered an error: ${error.message}`);
+      }
+    });
+};
+
 
 $(document).ready(() => {
 
-  // let tripId = currentTripId;
-
   $('#load-trips').click(loadTrips);
-  $('#one-trip').click(showTripDetails);
   $('#trip-list').on('click', 'button', function (event) {
     tripId = event.target.id;
     showTripDetails();
   });
-  
+  $('#trip-form').submit((event) => {
+    event.preventDefault();
+    createReservation();
+  });
 });
