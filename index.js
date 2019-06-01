495316bd3fafd6bac97887a731fee287661d3192
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-06-01 14:51:22 -0700

    trip details working

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
 const baseURL = "https://trektravel.herokuapp.com/trips";
 
-$("#see-trips").click( function() {
+const getAllTrips = () => {
+  let allTrips = [];
   axios.get(baseURL)
   .then((response) => {
     response.data.forEach((trip) =>
-    $(".trips").append(`<li>${trip.name}</li>`)
+    allTrips.push(trip)
   )})
-}) 
+  return allTrips;
+}
+
+const tripList = getAllTrips();
+
+const displayAllTrips = () => {
+    tripList.forEach((trip) =>
+    $("#all-trips-list").append(`<button id="${trip.id}">${trip.name}</button>`)
+  )
+};
+
+const displayTripDetails = (event) => {
+
+  // const getTripDetails = () => {
+  axios.get(baseURL + `/${event.target.id}`)
+  .then((response) => {
+    let details = response.data;
+    console.log(response.data);
+    $("#trip-details").empty();
+    if (details.id !== undefined) // won't display info if clicking in whitespace
+      $("#trip-details").append(`<h2>${details.name}</h2>`);
+      $("#trip-details").append(`<p>ID: ${details.id}</p>`);
+      $("#trip-details").append(`<p>${details.continent}</p>`);
+      $("#trip-details").append(`<p>${details.category}</p>`);
+      $("#trip-details").append(`<p>${details.weeks} weeks</p>`);
+      $("#trip-details").append(`<p>${details.about} </p>`);
+      $("#trip-details").append(`<p>$${details.cost} </p>`);
+  });
+
+  // return getTripDetails;
+}
+
+$(document).ready ( () => {
+  $("#see-trips").click(displayAllTrips);
+
+  $("#all-trips-list").on("click", displayTripDetails);
+});
+
+
+
+
