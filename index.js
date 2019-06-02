58258481ce0412a3a8b4fa52877f13bbf15db79d
Erica Norris <norrise120@gmail.com>
2019-06-02 16:58:25 -0700

    work to get the trips to load on click

---
+++ index.js
@@ -0,0 +1,32 @@
+const TREK_API = "https://trektravel.herokuapp.com/trips";
+
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+};
+
+const loadTreks = () => {
+  reportStatus('Loading treks...');
+
+  const trekList = $('#trek-list');
+  trekList.empty();
+
+  axios.get(TREK_API)
+    .then((response) => {
+      reportStatus(`Successfully loaded ${response.data.length} treks`);
+      response.data.forEach((trek) => {
+        trekList.append(`<li>${trek.name}</li>`);
+      });
+    })
+    .catch((error) => {
+      reportStatus(`Encountered an error while loading treks: ${error.message}`);
+      console.log(error);
+    });
+};
+
+
+$(document).ready( function() {
+  $('button').click( function() {
+    loadTreks();
+  })
+});
+
