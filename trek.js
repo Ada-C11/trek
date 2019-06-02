f71807feae0afda89e31fd1c7212c6d7b6ae087c
Ariana Bray <aribray@gmail.com>
2019-06-02 12:38:16 -0700

    added styling

--- trek.js
+++ trek.js
@@ -1,5 +1,6 @@
 TRIP_URL = "https://trektravel.herokuapp.com/trips/"
 
+// error reporting so the user knows what went wrong
 const reportStatus = (message) => {
   $('#status-message').html(message);
 };
@@ -23,6 +24,7 @@ const reportApiError = (error) => {
   reportStatus(errorHtml);
 }
 
+// load all trips
 const loadTrips = () => {
   
   const tripList = $('#trip-list');
@@ -32,6 +34,8 @@ const loadTrips = () => {
     .then((response) => {
       response.data.forEach((trip) => {
         tripList.append(`<li>${trip.name}</li>`)
+
+        // within the first GET request response, nested function that shows details of a specific trip
         const showDetails = () => {
 
           const tripDetail = () => {
@@ -39,16 +43,18 @@ const loadTrips = () => {
             .then((response) => {
               const detail = $('.details')
               detail.empty();
-              
-              detail.html(`<h1>Trek Trip Details<h1> 
-                <h2>Trip Name: ${response.data.name}</h2>
-                <h3>Continent: ${response.data.continent}</h3>
-                <h3>Category: ${response.data.category}</h3>
-                <h3>Weeks: ${response.data.weeks}</h3>
-                <h3>Cost: $${response.data.cost}</h3>
-                <h3>About: </h3>
+
+              // populating HTML with details from the second GET request response
+              detail.html(`<h1>Is This the Trip of Your Dreams?<h1> 
+                <h2>Excursion Name: <span>${response.data.name}</span></h2>
+                <h3>Continent: <span>${response.data.continent}</span></h3>
+                <h3>Excursion Category: <span>${response.data.category}</span></h3>
+                <h3>Weeks of Travel: <span>${response.data.weeks}</span></h3>
+                <h3>Cost: <span>$${response.data.cost}</span></h3>
+                <h3>Why This Trek Is Amazing: </h3>
                 <p>${response.data.about}</p>`)
 
+                // displays the form only when a specific trip is clicked
                 const form = $('#trek-form')
                 form.html(`<h1>Book Your Trip Now!</h1>
                           <form id="trek-form">
@@ -67,35 +73,34 @@ const loadTrips = () => {
                               <input type="text" name="email" />
                             </div>
         
-                          <input type="submit" name="add-trek" value="Add Trek" />
+                          <input type="submit" name="add-trek" value="Book My Trek" />
                         </form>`)
 
+                  // function to perform the addTrek function on a click event.
+                  const addTrek = (event) => {
 
-                        const addTrek = (event) => {
+                    event.preventDefault();
+                        
+                    const trekData = readFormData();
+                        
+                    reportStatus("About to book your trip!");
+                    // uses the trip.id from our previous response     
+                    axios.post(`${TRIP_URL}${trip.id}/reservations`, trekData)
+                    .then((response) => {
+                       
+                      const trekID = response.data.id;
+                      reportStatus(`Successfully booked a new trip with ID ${trekID}`);
+                      clearForm();
+                    })
+                    .catch((error) => {
+                      reportApiError(error);
+                    })
+                    };
 
-                          event.preventDefault();
-                        
-                          const trekData = readFormData();
-                        
-                          reportStatus("About to book your trip!");
-                          
-                          axios.post(`${TRIP_URL}${trip.id}/reservations`, trekData)
-                            .then((response) => {
-                              console.log("Congrats! Your trip has been booked.", response);
-                        
-                              const trekID = response.data.id;
-                              reportStatus(`Successfully booked a new trip with ID ${trekID}`);
-                              clearForm();
-                            })
-                            .catch((error) => {
-                              reportApiError(error);
-                            })
-                        };
-
-                        $('#trek-form').submit(addTrek);
+                    $('#trek-form').submit(addTrek);
           })
           .catch((error) => {
-            reportStatus(`Encountered an error while loading treks: ${error.message}`);
+            reportStatus(`Houston, we have a problem: ${error.message}`);
             console.log(error);
           });
           
@@ -105,17 +110,18 @@ const loadTrips = () => {
         };
         const selectedTrip = showDetails(trip);
        
-        $('li:last').click(selectedTrip);
-
-
-        
+        $('li:last').click(selectedTrip);   
         
       }) 
    
     })
+    .catch((error) => {
+      reportStatus(`Houston, we have a problem: ${error.message}`);
+      console.log(error);
+    });
 }
 
-
+// reads the form data and turns it into a readable format for the addTrek function
 const readFormData = () => {
   const parsedFormData = {};
 
@@ -131,36 +137,12 @@ const readFormData = () => {
   return parsedFormData;
 };
 
+// clears the form after submission
 const clearForm = () => {
   $(`#trek-form input[name="name"]`).val('');
   $(`#trek-form input[name="age"]`).val('');
   $(`#trek-form input[name="email"]`).val('');
 }
-
-
-// const addTrek = (event) => {
-
-//   event.preventDefault();
-
-//   const trekData = readFormData();
-
-//   reportStatus("About to book your trip!");
-//   // console.log("About to book your trip!", trekData);
-
-//   // need to get trip id and append it to the url
-//   axios.post(TRIP_URL, trekData)
-//     .then((response) => {
-//       console.log("Congrats! Your trip has been booked.", response);
-
-//       const trekID = response.data.id;
-//       reportStatus(`Successfully booked a new trip with ID ${trekID}`);
-//     })
-//     .catch((error) => {
-//       reportApiError(error);
-//     })
-// };
-
-
 
 $(document).ready(() => {
   $('#load-trips').click(loadTrips);
