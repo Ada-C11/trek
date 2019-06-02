8222bd62b2a2c75b12098ae8ab7915ac67a9502f
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-06-01 19:35:08 -0700

    added post functionality to create reservation

--- index.js
+++ index.js
@@ -22,14 +22,79 @@ const loadTrips = () => {
               const URL = `https://trektravel.herokuapp.com/trips/${trip.id}` 
               axios.get(URL)
               .then((response) => {
-                $('#trip').append(`<table><tr><th>Trip Details</th></tr><tr><td><b>Name:</b> ${trip.name}</td></tr><tr><td><b>Continent:</b> ${trip.continent}</td></tr><tr><td><b>Category:</b> ${trip.category}</td></tr><tr><td><b>Weeks:</b> ${trip.weeks}</td></tr><tr><td><b>Cost:</b> $${trip.cost}</td></tr><tr><td><b>About:</b> Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</td></tr></table>`);
-                $('#form').append('hello');
+                $('#trip').append(`<table>
+                  <tr><th>Trip Details</th></tr>
+                  <tr><td><b>Name:</b> ${trip.name}</td></tr>
+                  <tr><td><b>Continent:</b> ${trip.continent}</td></tr>
+                  <tr><td><b>Category:</b> ${trip.category}</td></tr>
+                  <tr><td><b>Weeks:</b> ${trip.weeks}</td></tr>
+                  <tr><td><b>Cost:</b> $${trip.cost}</td></tr>
+                  <tr><td><b>About:</b> Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</td></tr>
+                </table>`);
+                $('#form').append(
+                  `<h1>Reserve this Trip</h1>
+                  <form id='reservation-form'>
+                  <label for='name'>Name</label>
+                  <input type='text' name='name'/>
+                  <label for='email'>Email</label>
+                  <input type='text' name='email'/>
+                  <input type='submit' name='reserve' value='Reserve'/>
+                </form>`
+                );
+
+                // https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/10-JavaScript/reference/axios/index.js credit to these notes
+
+                const readFormData = () => {
+                  const parsedFormData = {};
+
+                  const nameFromForm = $(`#reservation-form input[name='name']`).val();
+                  parsedFormData['name'] = nameFromForm ? nameFromForm : undefined;
+
+                  const emailFromForm = $(`#reservation-form input[name='email']`).val();
+                  parsedFormData['email'] = emailFromForm ? emailFromForm : undefined;
+
+                  return parsedFormData;
+                };
+
+                const clearForm = () => {
+                  $(`#reservation-form input[name='name']`).val('');
+                  $(`#reservation-form input[name='email']`).val('');
+                }
+
+                const createReservation = (event) => {
+                  event.preventDefault();
+
+                  const reservationData = readFormData();
+                  console.log(reservationData);
+
+                  reportStatus('Sending reservation data...');
+                  const URL = `https://trektravel.herokuapp.com/trips/${trip.id}/reservations`
+
+                  axios.post(URL, reservationData)
+                    .then((response) => {
+                      reportStatus(`Successfully added a reservation with ID ${response.data.id} for ${trip.name}!`);
+                      clearForm();
+                    })
+                    .catch((error) => {
+                      console.log(error.response);
+                      if (error.response.data && error.response.data.errors) {
+                        reportStatus(
+                          `Encountered an error: ${error.message}`,
+                          error.response.data.errors
+                        );
+                      } else {
+                        reportStatus(`Encountered an error: ${error.message}`);
+                      }
+                    });
+                };
+                $('#reservation-form').submit(createReservation);
               })
               .catch((error) => {
-                reportStatus(`Encountered an error while loading pets: ${error.message}`);
+                reportStatus(`Encountered an error while loading s: ${error.message}`);
                 console.log(error);
               });
             }
+
         $('#trips').append(`<tr><td id='${trip.id}'>${trip.name}</td></tr>`);
 
         $(`#${trip.id}`).one('click', loadOneTrip);
@@ -42,10 +107,11 @@ const loadTrips = () => {
       });
     })
     .catch((error) => {
-      reportStatus(`Encountered an error while loading pets: ${error.message}`);
+      reportStatus(`Encountered an error while loading reservations: ${error.message}`);
       console.log(error);
     });
 }
+
 
 $(document).ready(() => {
   $('#load').click(loadTrips);
