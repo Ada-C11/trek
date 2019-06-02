15217a05ed80b74095b1d8b24c7e5f89c0a39ed5
Maria Wissler <wmcarmelina@gmail.com>
2019-06-02 16:16:46 -0700

    started wave 2, links to table elements to build trip details

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 const URL_ALL_TRIPS = "https://trektravel.herokuapp.com/trips"
+var urlDynamic=URL_ALL_TRIPS;
 
 $(document).ready(function(){
     $("#btnAllTrips").on("click",function(){
-        GetAllTrips();
+        getAllTrips();
     });
 });
 
-function GetAllTrips(){
+function getAllTrips(){
     axios.get(URL_ALL_TRIPS)
     .then(function(response){
+        $("#detailTrip").addClass("invisible");
+        $("#reserveTrip").addClass("invisible");
         buildList(response.data);
     }).catch(function(error){
-        console.log("El Error es: "+error.response);
+        alert("Sorry an error ocurred, please try again.");
     });
 }
 
 function buildList(trips){
     var list = $("#tripList tbody");
+    list.empty();
     $.each(trips,function(index,item){
         list.append($("<tr>")
-             .append($("<td>")
-                .append($("<span>")
-                    .append("Trip "+item.id + " " +item.name))));
+                .append($("<td>")
+                    .append($("<button>")
+                        .addClass("btn btn-default viewDetail")
+                        .data("idTrip",item.id)
+                        .append("Trip "+item.id + " " +item.name)
+                    )
+                )
+            );
+    });
+
+    $(".viewDetail").bind("click",function(){
+        var tripid = $(this).data("idTrip");
+        getTrip(tripid);
     });
 }
+
+
