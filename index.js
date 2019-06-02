26953bc0d093d6db81b96b4d5996fd7fcfae20a2
Jansen <v.jansen.martin@gmail.com>
2019-06-02 15:02:53 -0700

    Revise registration event for reservation form to account for dynamically-created elements with .on

--- index.js
+++ index.js
@@ -4,16 +4,37 @@ const reportStatus = (message) => {
     $('#status-message').html(message);
 };
 
+const makeReservation = (event) => {
+    event.preventDefault();
+    const reservationUrl = "https://trektravel.herokuapp.com/trips/70/reservations";
+    const reservationData = {
+        name: "Jax Wheeler",
+        email: "jwheeler@wheelieeees"
+    };
+
+    console.log(reservationData);
+
+    axios.post(reservationUrl, reservationData)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
+
 const loadReservationForm = () => {
+  $('.reservations').append('<h2>Reserve a Trip</h2>')
   $('.reservations').append('<form id="reservation-form"></form>')
-  
+
+
     const nameField = '<div>' + '<label for="name">Name:</label>' + 
     '<input type="text" id="name" name="user_name">' + '</div>'
 
     const emailField = '<div>' + '<label for="email">Email:</label>' + 
     '<input type="text" id="email" name="user_email">' + '</div>'
 
-    const submitButton = '<div class="button">' + 
+    const submitButton = '<div class="button" id="reserve">' + 
     '<button type="submit">Reserve</button>' + '</div>'
   
     $('#reservation-form').append(nameField);
@@ -68,6 +89,7 @@ const loadTrips = () => {
 
 $(document).ready(() => {
     $('#load').click(loadTrips);
+
     $('#trip-list').on('click', 'li', function() {
         $('.details').empty();
         $('.reservations').empty();
@@ -76,8 +98,10 @@ $(document).ready(() => {
         const tripId = parseInt($(this).attr("id"));
         const detailsWindow = '<ul id="details-list"></ul>';
 
-        // const detailsWindow = '<ul class="details-list">' + `<li>${$(this).html()} number ${tripId}</li>` + '</ul>'
         $('section.details').append(detailsWindow);
         loadDetails(tripId);
     });
+
+    $(document).on("submit", "#reservation-form", makeReservation);
+    // $('#reservation-form').submit(makeReservation);
 })
