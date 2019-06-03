907130c68940741d17bd8d33e9f2ec3f6e2147d2
Carla Bosco <carlabosco@gmail.com>
2019-06-02 20:14:17 -0700

    Started working on reservation form

--- trek.js
+++ trek.js
@@ -17,41 +17,55 @@ const reportError = (message, errors) => {
   reportStatus(content);
 };
 
-
-const displayTrips = (tripList) => {
-  const target = $(`#trip-list`);
+const showTrip = (trip) => {
+  const target = $('#trip-details');
   target.empty();
-  tripList.forEach(trip => {
-    target.append(`<li><a class='trip-item' href='#'>${trip.name}</a></li>`);
-  })
+  target.append(
+    `<li>ID: ${trip.id}</li>
+    <li>Name: ${trip.name}</li>
+    <li>Category: ${trip.category}</li>
+    <li>Continent: ${trip.continent}</li>
+    <li>Cost: ${trip.cost}</li>
+    <li>Duration in weeks: ${trip.weeks}</li>`
+  )
 }
 
-const showTrip = (trip) => {
-  $('body').on('click', '.trip-item', function(event) {
-    $('#trip-details').append(tripDetails);
-  });
+const callTrip = (id) => {
+  
+  const loadTrip = () => {
+    axios.get(tripsURL+`/${id}`)
+      .then((response) => {
+        showTrip(response.data);
+      })
+      .catch((error) => {
+        reportStatus(`Encountered an error while loading trips: ${error.message}`);
+        console.log(error);
+      });
+  }
+  return loadTrip;
 };
-
-// const tripDetails = $(
-//   `<li>ID: ${trip.id}</li>
-//   <li>Category: ${trip.category}</li>
-//   <li>Continent: ${trip.continent}</li>
-//   <li>Cost: ${trip.cost}</li>
-//   <li>Name: ${trip.name}</li>
-//   <li>Duration: ${trip.weeks}</li>`
-//   )
-
-
 
 //
 const loadTrips = () => {
+  const target = $(`#trip-list`);
+  target.empty();
+  
   reportStatus('Loading trips...');
 
   axios.get(tripsURL)
   .then((response) => {
     const trips = response.data;
-    displayTrips(trips);
-    reportStatus(`Successfully loaded ${response.data.length} trips`);
+
+    reportStatus(`Successfully loaded ${trips.length} trips`);
+
+    trips.forEach((trip) => {
+      target.append(`<li class='trip-item'><a href='#'>${trip.name}</a></li>`);
+      
+      const tripClickHandler = callTrip(trip.id);
+      // console.log(`${trip.id}`)
+      $(`.trip-item`).on('click', tripClickHandler);
+    })
+    
   })
 
   .catch((error) => {
@@ -60,8 +74,58 @@ const loadTrips = () => {
   });
 };
 
+//
+// Creating trips
+//
+const readFormData = () => {
+  const parsedFormData = {};
+
+  const nameFromForm = $(`#trip-form input[name="name"]`).val();
+  parsedFormData['name'] = nameFromForm ? nameFromForm : undefined;
+
+  const emailFromForm = $(`#trip-form input[name="email"]`).val();
+  parsedFormData['email'] = emailFromForm ? emailFromForm : undefined;
+
+  return parsedFormData;
+};
+
+const clearForm = () => {
+  $(`#trip-form input[name="name"]`).val('');
+  $(`#trip-form input[name="email"]`).val('');
+}
+
+const createTrip = (event) => {
+  // Note that createTrip is a handler for a `submit`
+  // event, which means we need to call `preventDefault`
+  // to avoid a page reload
+  event.preventDefault();
+
+  const tripData = readFormData();
+  console.log(tripData);
+
+  reportStatus('Sending trip data...');
+
+  axios.post(tripsURL, tripData)
+    .then((response) => {
+      reportStatus(`Successfully added a trip with ID ${response.data.id}!`);
+      clearForm();
+    })
+    .catch((error) => {
+      console.log(error.response);
+      if (error.response.data && error.response.data.errors) {
+        reportError(
+          `Encountered an error: ${error.message}`,
+          error.response.data.errors
+        );
+      } else {
+        reportStatus(`Encountered an error: ${error.message}`);
+      }
+    });
+};
+
+
 
 $(document).ready(() => {
   $('#load').click(loadTrips);
-  // $('#trip-form').submit(createTrip);
+  $('#trip-form').submit(createTrip);
 });
