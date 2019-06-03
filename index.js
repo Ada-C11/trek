5649ee04b317ddc9f26f340b10e8fc06a74f924a
Mello-Cello <celloelle@gmail.com>
2019-06-02 21:36:11 -0700

    Adjusted grid layout and HTML to accomodate form. Began implementation of function to make reservation. Need to bug fix.

--- index.js
+++ index.js
@@ -31,13 +31,14 @@ const loadTrips = () => {
             listOfTrips.append(`<tr id=${trip.id}><td>${trip.name}</td></tr>`);
 
             $(`#${trip.id}`).bind('click', {thisTrip:trip}, function(event){
-              // {trip:trip} becomes available as event.data
+                // {trip:trip} becomes available as event.data
                 // console.log(event);
                 // console.log("******* trip is: ********");
                 // console.log(event.data.trip);
                 // console.log('***********trip***********');
 
                 const trip = event.data.thisTrip;
+
                 $("#trip-details").empty();
                 $("#trip-details").append(`<h2>${trip.name}</h2>`);
                 $("#trip-details").append(`<p>ID: ${trip.id}</p>`);
@@ -46,7 +47,61 @@ const loadTrips = () => {
                 $("#trip-details").append(`<p>${trip.weeks} weeks</p>`);
                 $("#trip-details").append(`<p>Summary: ${trip.about} </p>`);
                 $("#trip-details").append(`<p>Price: $${trip.cost} </p>`);
+                
+                $('#reservation-form').empty();
+                $('#reservation-form').prepend('<h2>Reserve your spot!</h2>')
+                $('#reservation-form').append('<div> <label for="name">Name</label> <input type="text" name="name"/> </div>');
+                $('#reservation-form').append('<div> <label for="email">Email Address</label> <input type="text" name="email"/> </div>');
+                $('#reservation-form').append('<input type="submit" class="btn btn-outline-info" name="Make Reservation"/>');
+                
+                return trip // need this?????
             });
+
+            const readFormData = () => {
+                const formData = {};
+
+                const nameFromForm = $(`#reservation-form input[name="name"]`).val();
+                formData['name'] = nameFromForm ? nameFromForm : undefined;
+
+                const emailFromForm = $('#reservation-form input[email="email"]').val();
+                formData['email'] = emailFromForm ? emailFromForm : undefined;
+
+                return formData;
+            };
+
+            const clearForm = () => {
+                $(`#reservation-form input [name="name"]`).val('');
+                $(`#reservation-form input [email="email"]`).val('');
+            };
+
+            const reserveTrip = (event) => {
+                event.preventDefault();
+
+                const reservationData = readFormData();
+                console.log(reservationData);
+
+                reportStatus('Submitting reservation...');
+
+                axios.post(`${BASEURL}/${trip.id}/reservations`, reservationData)
+                    .then((response) => {
+                        reportStatus(`Successfully reserved your trip. name: ${response.data.name}  email: ${response.data.email}`);
+                        clearForm();
+                    })
+                    .catch((error) => {
+                        console.log(error.response);
+                        if (error.response.data && error.response.data.errors) {
+                            reportError(`Encountered an error: ${error.message}`, error.response.data.errors);
+                          } else {
+                            reportStatus(`Encountered an error: ${error.message}`);
+                          }
+                    });
+
+                // accepted params:
+                // name (string) required
+                // age (integer)
+                // email (string) required
+            };
+
 
         });//END Each loop
 
@@ -55,14 +110,6 @@ const loadTrips = () => {
         reportStatus(`Error loading trips: ${error.message}`);
         console.log(error);
     });
-};
-
-
-const reserveTrip = (trip) => {
-    console.log("reserving trip", trip)
-
-    // TODO: Wave 3
-    // reserve a spot on the trip when the form is submitted
 };
 
 
