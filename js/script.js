b7c229f453a0d29eb77295f425e8b0a3a25bd3b0
Sopheary Chiv <sophearychiv@gmail.com>
2019-06-02 23:12:31 -0700

    Create a new reservation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 let tripData = {};
+let selectedTrip = null;
 
 const displayStatus = (message) => {
     $('#status').html(message);
   }
   
 const handleApiError = (error) => {
-console.log(error);
-// TODO: politely report this error to the user
+    displayStatus(error);
 }
 
 const loadTrips = () => {
@@ -16,12 +16,14 @@ displayStatus("loading trips...");
     $("#trip-list")[0].innerHTML = getAlltrips(response.data);
     displayStatus("");
     $('#trip-list div').bind("click", showTripDetails);
-    })();
+    })().catch(function(error) {
+        handleApiError("There is an issue while pulling the trip information");
+    });
 }
 
 const showTripDetails = (trip) => {
 const tripName = trip.currentTarget.textContent;
-const selectedTrip = tripData.filter(o => o.name === tripName);
+selectedTrip = tripData.filter(o => o.name === tripName);
 if (selectedTrip && selectedTrip.length > 0) {
     const tripId = selectedTrip[0].id;
     displayStatus("loading trips...");
@@ -29,22 +31,51 @@ if (selectedTrip && selectedTrip.length > 0) {
         const response = await axios.get('https://trektravel.herokuapp.com/trips/' + tripId);
         $("#trip-details")[0].innerHTML = getTripDetails(response.data);
         displayStatus("");
-        })();
+        $('#reserve-trip').show();
+        })().catch(function(error) {
+            handleApiError("There is an issue while pulling the trip's details");
+        });
 }
 }
 
-const reserveTrip = (trip) => {
-console.log("reserving trip", trip)
+const reserveTrip = () => {
+    if (selectedTrip && selectedTrip.length > 0) {
+        const name = $("#txtName");
+        const email = $("#txtEmail");
+        const tripId = selectedTrip[0].id;
+        if (validateData(name.val()) && validateData(email.val())) {
+            const newReservedData = {
+                name: name.val(),
+                email: email.val()
+              }
+            displayStatus("loading...");
+            (async () => {
+                await axios.post('https://trektravel.herokuapp.com/trips/'+ tripId +'/reservations', newReservedData);
+                displayStatus("The trip is reserved");
+                name.val('');
+                email.val('');
+                })().catch(function (error) {
+                    handleApiError('There is an issue while creating a new reservation');
+                });
+        }
+    }
+}
 
-// TODO: Wave 2
-// reserve a spot on the trip when the form is submitted
+const validateData = (data) => {
+    if (!data || data === '') {
+        handleApiError("Name or email is not correct");
+        return false;
+    }
+    return true;
 }
 
 $(document).ready(() => {
     $('#load-trips').click(loadTrips);
+    $('#reserve-trip-button').click(reserveTrip);
+    $('#reserve-trip').hide();
 });
 
-function getAlltrips(data) {
+const getAlltrips = (data) => {
     tripData = data;
     let output = '<h4>All Trips</h4>';
     data.forEach(function(trip){
@@ -53,7 +84,7 @@ function getAlltrips(data) {
     return output;
 }
 
-function getTripDetails(data) {
+const getTripDetails = (data) => {
     let output = '<h4>Trip Details</h4>' +
                 '<div><label>Name: </label>' + data.name +'</div>' +
                 '<div><label>Continent: </label>' + data.continent +'</div>' + 
