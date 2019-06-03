16ba43b34bf4bbf7c1808a4a3001b5b27135deef
Sopheary Chiv <sophearychiv@gmail.com>
2019-06-02 21:47:14 -0700

    Show trip's details when user click on each trip

--- js/script.js
+++ js/script.js
@@ -15,14 +15,22 @@ displayStatus("loading trips...");
     const response = await axios.get('https://trektravel.herokuapp.com/trips');
     $("#trip-list")[0].innerHTML = getAlltrips(response.data);
     displayStatus("");
+    $('#trip-list div').bind("click", showTripDetails);
     })();
 }
 
 const showTripDetails = (trip) => {
-console.log("showing details for trip", trip);
-
-// TODO: Wave 2
-// display trip details and the trip reservation form
+const tripName = trip.currentTarget.textContent;
+const selectedTrip = tripData.filter(o => o.name === tripName);
+if (selectedTrip && selectedTrip.length > 0) {
+    const tripId = selectedTrip[0].id;
+    displayStatus("loading trips...");
+    (async () => {
+        const response = await axios.get('https://trektravel.herokuapp.com/trips/' + tripId);
+        $("#trip-details")[0].innerHTML = getTripDetails(response.data);
+        displayStatus("");
+        })();
+}
 }
 
 const reserveTrip = (trip) => {
@@ -42,5 +50,17 @@ function getAlltrips(data) {
     data.forEach(function(trip){
         output += '<div>' + trip.name + '</div>';
     });
+    return output;
+}
+
+function getTripDetails(data) {
+    let output = '<h4>Trip Details</h4>' +
+                '<div><label>Name: </label>' + data.name +'</div>' +
+                '<div><label>Continent: </label>' + data.continent +'</div>' + 
+                '<div><label>Category: </label>' + data.category +'</div>' + 
+                '<div><label>Weeks: </label>' + data.weeks +'</div>' + 
+                '<div><label>Cost: </label>' + data.cost +'</div>' + 
+                '<div><label>About: </label><br>' + data.about + '</br></div>';
+    
     return output;
 }
