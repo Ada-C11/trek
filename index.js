1cec18fd8808f4b5bb6c2f381d29a056364f329a
kateannnichols <kateannnichols@gmail.com>
2019-06-02 20:19:32 -0700

    can view trip details from trip name

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const tripsUrl = 'https://trektravel.herokuapp.com/trips';
 
 const displayStatus = (message) => {
@@ -21,55 +20,70 @@ const displayStatus = (message) => {
   //   reportStatus(content);
   // };
 
+  const showTripDetails = id => {
+    const detailsUrl = tripsUrl + `/${id}`;
+    axios.get(detailsUrl)
+      .then(function(response) {
+          const trip = response.data;
+          let info = `<h3> ${trip.name}</h2>
+          <p>${trip.about}</p>
+          <ul>
+          <li><b>Continent</b>: ${trip.continent}</li>
+          <li><b>Category</b>: ${trip.category}</li>
+          <li><b>Duration</b>: ${trip.weeks} weeks</li>
+          <li><b>Cost</b>: $${trip.cost}</li>
+          <li><b>Trip ID</b>: ${trip.id}</li>
+          </ul>
+          <button id="reserve">Reservation form</button>`;
+          $("#trip-info").append(info);
+        })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
+
 // makes an axios call to the trips index and display the results
 const loadTrips = () => {
     const tripList = $('#trip-list');
     tripList.empty();
+    $("#trip-info").empty();
 
     axios.get(tripsUrl)
       .then(function (response) {
         response.data.forEach((trip) => {
             tripList.append(`<li><a href="" class="trip-link" id="${trip.id}">${trip.name}</a></li>`);
-          });
-
-          console.log(response);
+        });
+        console.log(response);
+        })
         // resultElement.innerHTML = generateSuccessHTMLOutput(response);
-      })
       .catch(function (error) {
           console.log(error);
         // resultElement.innerHTML = generateErrorHTMLOutput(error);
       });   
   }
 
-  const showTripDetails = (trip) => {
-    console.log("showing details for trip", trip);
 
-    const tripDetails = $('#trip-details');
-    tripDetails.empty();
-
-    axios.get(tripsUrl)
-      .then(function (response) {
-        response.data.forEach((trip) => {
-          tripDetails.append(`<li>${trip.id}</li>`);
-        });
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });   
-    } 
-  
-    // TODO: Wave 2
-    // display trip details and the trip reservation form
-  // }
-  
   const reserveTrip = (trip) => {
     console.log("reserving trip", trip)
-  
-    // TODO: Wave 2
-    // reserve a spot on the trip when the form is submitted
   }
-  
-  $(document).ready(() => {
-    $('#load-trips').click(loadTrips);
+
+  $(document).on("click", "a", event => {
+    event.preventDefault();
+    $('.trip-details').show();
+    const id = $(event.target).attr("id");
+    console.log("loading " + id);
+    showTripDetails(id);
+    $('#trip-list').hide();
   });
+
+  $(document).on("click", "#load-trips", event => {
+    $('.trip-details').hide();
+    $('#trip-list').show();
+    loadTrips();
+  });
+  
+  $(document).ready(() => {  
+    $('.trip-details').hide();
+    $('#load-trips').click(loadTrips);
+}) 
