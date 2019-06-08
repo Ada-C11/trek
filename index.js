8e1f9220963a338fd53603972ab94b863b5843f4
Mello-Cello <celloelle@gmail.com>
2019-06-08 16:17:15 -0700

    Rearranged error message handling so it is all done by one function. Added required keyword to HTML form to prevent unnecessary API calls. Added some comments on on items that still need attention.

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const reportStatus = (message) => {
     $('#status-message').html(message);
 };
 
-const handleApiError = (error) => {
-    console.log(error);
+const handleApiError = (error, customMessage) => {
+    // console.log(error);
     // TODO: politely report this error to the user
+
+    console.log(error.response);
+    if (error.response.data && error.response.data.errors) {
+        reportStatus(`${customMessage} Details: ${error.message}, ${error.response.data.errors}`);
+      } else {
+        reportStatus(`${customMessage} Details: ${error.message}`);
+      }
 };
 
 // send GET request
 // data comes back, store in an object
 // loop through the object and display each item on the page
 
-let allTrips;
+let allTrips; // not sure this is needed
 
 const loadTrips = () => {
     reportStatus('Loading trips ...');
@@ -44,6 +51,8 @@ const loadTrips = () => {
                 //set class='selected' on this table row
                 $(`#${trip.id}`).addClass('selected');
 
+                // **** consider building the form once ****
+                // **** hide on page load and unhide when a trip is clicked ****
 
                 $("#trip-details").empty();
                 $("#trip-details").append(`<h2>${trip.name}</h2>`);
@@ -58,8 +67,9 @@ const loadTrips = () => {
                 $('#reservation-form').unbind('submit');
 
                 $('#reservation-form').prepend('<h2>Reserve your spot!</h2>')
-                $('#reservation-form').append('<div> <label for="guestname">Name</label> <input type="text" name="guestname"/> </div>');
-                $('#reservation-form').append('<div> <label for="email">Email Address</label> <input type="text" name="email"/> </div>');
+                $('#reservation-form').append('<div> <label for="guestname">Name</label> <input type="text" name="guestname" required/> </div>');
+                $('#reservation-form').append('<div> <label for="email">Email Address</label> <input type="text" name="email" required/> </div>');
+                $('#reservation-form').append('<div> <label for="age">Age</label> <input type="text" name="age"/> </div>');
                 $('#reservation-form').append('<input type="submit" class="btn btn-outline-info" name="make-reservation" value="Make Reservation"/>');
                 // $('submit-btn').click(makeReservation);
                 $('#reservation-form').submit(reserveTrip);
@@ -73,8 +83,11 @@ const loadTrips = () => {
 
     })
     .catch((error) => {
-        reportStatus(`Error loading trips: ${error.message}`);
-        console.log(error);
+        // reportStatus(`Error loading trips: ${error.message}`);
+        // console.log(error);
+        handleApiError(error, "We encountered a problem loading the list of available trips.");
+
+        
     });
 
 };
@@ -85,15 +98,20 @@ const readFormData = () => {
     const nameFromForm = $(`#reservation-form input[name="guestname"]`).val();
     formData['name'] = nameFromForm ? nameFromForm : undefined;
 
-    const emailFromForm = $('#reservation-form input[name="email"]').val();
+    const emailFromForm = $(`#reservation-form input[name="email"]`).val();
     formData['email'] = emailFromForm ? emailFromForm : undefined;
+
+    const ageFromForm = $(`#reservation-form input[name="age"]`).val();
+    formData['age'] = ageFromForm ? ageFromForm : 'not provided';
 
     return formData;
 };
 
+// **** THIS ISN'T WORKING, NEED TO FIX!! ****
 const clearForm = () => {
     $(`#reservation-form input [name="guestname"]`).val('');
     $(`#reservation-form input [email="email"]`).val('');
+    $(`#reservation-form input [age="age"]`).val('');
 };
 
 const reserveTrip = (event) => {
@@ -111,16 +129,13 @@ const reserveTrip = (event) => {
 
     axios.post(`${BASEURL}/${tripID}/reservations`, reservationData)
         .then((response) => {
-            reportStatus(`Successfully reserved your trip. name: ${response.data.name}  email: ${response.data.email}`);
+            const tripName = $('.selected').attr('name'); //shows as undefined
+            reportStatus(`Successfully reserved your trip ${tripName}. (name: ${response.data.name}  email: ${response.data.email} age: ${response.data.age}`);
             clearForm();
         })
         .catch((error) => {
-            console.log(error.response);
-            if (error.response.data && error.response.data.errors) {
-                reportError(`Encountered an error: ${error.message}`, error.response.data.errors);
-              } else {
-                reportStatus(`Encountered an error: ${error.message}`);
-              }
+            handleApiError(error, "We encountered a problem submitting a reservation.");
+            clearForm();
         });
 
     // accepted params:
