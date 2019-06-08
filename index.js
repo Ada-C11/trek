78eacce64c2b46f542e8b7611129d915bb5a415b
Margaret Finnan <msfinnan@gmail.com>
2019-06-08 12:10:47 -0700

    html/css styling

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const displayStatus = (message) => {
   $('#status').html(message);
 }
 
-let tripId;
+
 
 const reportError = (message, errors) => {
   let content = `<p>${message}</p><ul>`;
@@ -34,21 +34,55 @@ const loadTrips = () => {
     });
 }
 
-const showTripDetails = () => {
-  axios.get(`${BASE_URL}/${tripId}`) 
+const buildSubmitHandler = (tripId) => {
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+    const reservationData = readFormData();
+
+    displayStatus('Reserving trip...');
+    console.log('reservationdata is', reservationData)
+
+    axios.post(`${BASE_URL}/${tripId}/reservations`, reservationData)
+      .then((response) => {
+        console.log('posted res data', response);
+        displayStatus(`Successfully created a reservation with ID:${response.data.id}`);
+        clearForm();
+      })
+      .catch((error) => {
+        console.log(error.response);
+        if (error.response.data && error.response.data.errors) {
+          reportError(
+            `Encountered an error: ${error.message}`,
+            error.response.data.errors
+          );
+          console.log(error)
+        } else {
+          displayStatus(`Encountered an error: ${error.message}`);
+        }
+      });
+
+
+  }
+  return handleSubmit;
+}
+
+const showTripDetails = (tripId) => {
+  axios.get(`${BASE_URL}/${tripId}`)
     .then((response) => {
       const trip = response.data;
       displayStatus('Successfully loaded trip');
       console.log(response.data);
       $('#trip-details').empty();
-      $('#trip-details').append('<h2>Trip Details</h2>');
-      $('#trip-details').append(`<li>Name: ${trip.name}</li>`);
-      $('#trip-details').append(`<li>Continent: ${trip.continent}</li>`);
-      $('#trip-details').append(`<li>Category: ${trip.category}</li>`);
-      $('#trip-details').append(`<li>Cost: ${trip.cost}</li>`);
-      $('#trip-details').append(`<li>About: ${trip.about}</li>`);
+      $('#trip-details').append('<h2><strong> Trip Details </strong></h2>');
+      $('#trip-details').append(`<li><strong> Name: </strong> ${trip.name}</li>`);
+      $('#trip-details').append(`<li><strong> Continent: </strong> ${trip.continent}</li>`);
+      $('#trip-details').append(`<li><strong> Category: </strong> ${trip.category}</li>`);
+      $('#trip-details').append(`<li><strong> Cost: </strong> $${trip.cost}</li>`);
+      $('#trip-details').append(`<li><strong> About: </strong> ${trip.about}</li>`);
       $('#trip-details').append(`<form id="trip-form">
-        <h1>Reserve a Spot on ${trip.name}</h1>
+        <h2><strong> Reserve a Spot on ${trip.name} </strong></h2>
         <div>
           <label for="name">Name</label>
           <input type="text" name="name" />
@@ -58,9 +92,9 @@ const showTripDetails = () => {
           <input type="text" name="email" />
         </div>
 
-        <input type="submit" name="trip-form" value="Submit Reservation" />
+        <input type="button" class="btn btn-primary" type="submit" name="trip-form" value="Submit Reservation" />
       </form>`)
-      $('#trip-form').submit(addReservation);
+      $('#trip-form').submit(buildSubmitHandler(trip.id));
 
     })
     .catch((error) => {
@@ -75,51 +109,51 @@ const readFormData = () => {
   const nameFromForm = $(`#trip-form input[name="name"]`).val();
   parsedFormData['name'] = nameFromForm ? nameFromForm : undefined;
 
-  const emailFromForm = $(`#trip-form input[email="email"]`).val();
+  const emailFromForm = $(`#trip-form input[name="email"]`).val();
   parsedFormData['email'] = emailFromForm ? emailFromForm : undefined;
   //when submitting post request, email is showing as undefined!? why?? 
   return parsedFormData;
-  
+
 };
 
 const clearForm = () => {
   $(`#trip-form input[name="name"]`).val('');
-  $(`#trip-form input[email="email"]`).val('');
+  $(`#trip-form input[name="email"]`).val('');
 }
 
-const addReservation = (event) => {
-  event.preventDefault();
-  const reservationData = readFormData();
+// const addReservation = (event) => {
+//   event.preventDefault();
+//   const reservationData = readFormData();
 
-  displayStatus('Reserving trip...');
-  console.log('reservationdata is', reservationData)
+//   displayStatus('Reserving trip...');
+//   console.log('reservationdata is', reservationData)
 
-  axios.post(`${BASE_URL}/${tripId}/reservations`, reservationData)
-    .then((response) => {
-      console.log('posted res data', response);
-      displayStatus(`Successfully created a reservation with ID:${response.data.id}`);
-      clearForm();
-    })
-    .catch((error) => {
-      console.log(error.response);
-      if (error.response.data && error.response.data.errors) {
-      reportError(
-        `Encountered an error: ${error.message}`,
-        error.response.data.errors
-      );
-      console.log(error)
-      } else {
-      displayStatus(`Encountered an error: ${error.message}`);
-      }
-    });
-};
+//   axios.post(`${BASE_URL}/${tripId}/reservations`, reservationData)
+//     .then((response) => {
+//       console.log('posted res data', response);
+//       displayStatus(`Successfully created a reservation with ID:${response.data.id}`);
+//       clearForm();
+//     })
+//     .catch((error) => {
+//       console.log(error.response);
+//       if (error.response.data && error.response.data.errors) {
+//       reportError(
+//         `Encountered an error: ${error.message}`,
+//         error.response.data.errors
+//       );
+//       console.log(error)
+//       } else {
+//       displayStatus(`Encountered an error: ${error.message}`);
+//       }
+//     });
+// };
 
 
 $(document).ready(() => {
 
   $('#load-trips').click(loadTrips);
   $('#trip-list').on('click', 'button', function (event) {
-    tripId = event.target.id;
-    showTripDetails();
+    const tripId = event.target.id;
+    showTripDetails(tripId);
   });
 });
