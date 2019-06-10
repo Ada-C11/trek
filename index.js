d45142234ceba037eaa96931bae8496a27689c01
laneia <laneiamurray@gmail.com>
2019-06-09 18:59:40 -0700

    added report status and error functionality

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const TRIPS = 'https://trektravel.herokuapp.com/trips';
+
+const reportStatus = (message) => {
+    $('#status-message').html(message);
+  };
+
+const reportError = (message, errors) => {
+    let content = `<p>${message}</p><ul>`;
+        for (const field in errors) {
+            for (const problem of errors[field]) {
+                content += `<li>${field}: ${problem}</li>`;
+                }
+        }
+    content += "</ul>";
+    reportStatus(content);
+  };
